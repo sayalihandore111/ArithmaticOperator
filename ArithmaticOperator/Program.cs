706d007f9601a339c17557753f2e5a39b2336b84
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 20:38:46 +0530

    Add project files.

---
+++ ArithmaticOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArithmaticOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            num1 = 4;
+            num2 = 5;
+            sum = num1 + num2;
+            Console.WriteLine("The value of first variable is {0} ", num1);
+            Console.WriteLine("The value of second variable is " + num2);
+            Console.WriteLine("The addition of {0} and {1} = {2}", num1, num2, sum);
+            Console.WriteLine("{0} is the total of {1} and {2}", sum, num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
